18fadfd4a24e73740fbc4997baa48553b1a1ae98
Corey <coreyzhee@gmail.com>
2022-11-13 21:50:53 -0500

    commit tree ac9ebbe14ec51bfb0672bd641f20d3c4db986089

---
+++ index.js
@@ -0,0 +1,16 @@
+let num1 = 2000
+let num2 = 100
+let x = 2
+let a = 3
+let b = 4
+
+const divide = (num1, num2) => num1 / num2 
+   
+divide(num1,num2)
+
+const square = (x) => x * x
+multiply(x,x)
+
+const add = (a, b) => a + b
+console.log(add(a,b))
+
